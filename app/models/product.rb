9b93947a63c8ac56c2e77d3d21fe7666186fc001
Joseph Hassan <konaman@gmail.com>
2013-12-14 14:28:45 -0800

    Shopping cart view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_line_item
+  
+  private
+  def ensure_not_referenced_by_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
   # insure that the user has provided a title, description and an image URL
   validates :title, :description, :image_url, presence: true
   
