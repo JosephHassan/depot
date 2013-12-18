03ba3357d23e25d01f891b73577e97917f1de9d8
Joseph Hassan <konaman@gmail.com>
2013-12-18 15:49:48 -0800

    completing checkout functionality

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -12,6 +12,11 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
     get :new
     assert_response :success
   end
@@ -21,7 +26,7 @@ class OrdersControllerTest < ActionController::TestCase
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path #order_path(assigns(:order))
   end
 
   test "should show order" do
@@ -46,4 +51,11 @@ class OrdersControllerTest < ActionController::TestCase
 
     assert_redirected_to orders_path
   end
+  
+  test "require item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Why are you here? Your cart is empty!!'
+  end
+  
 end
